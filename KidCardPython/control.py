5dde20f7e0b4a4d864c595d2160d59e8c15f689b
Nathan Robbins <nathancrobbins@gmail.com>
2017-10-28 12:23:13 -0600

    Fixed method errors.

--- KidCardPython/control.py
+++ KidCardPython/control.py
@@ -1,38 +1,48 @@
 import testing
 
 
-#The login process
-def Login(username, password)
-	#Get primary accountNo from database
-	#If not found: return to index
+#Create a new primary account, returns the Galileo ID
+def CreatePrimaryAccount(firstName, lastName):
+	fields{'firstName': firstName, 'lastName': lastName}
+	accountNo = CreateAccount(fields)
+	ActivateAccount(accountNo)
+	return accountNo
+
+#Get Children and Balance
+def GetChildren(accountNo):
 	
-	parentbalance = GetAccountBalance(accountNo)
+	children = {}
 	
 	for account in GetRelatedAccounts(accountNo)
 		name = GetAccountHolderName(account)
 		balance = GetAccountBalance(account)
+		children[name] = balance
 
-#Create a new child card for the given parent account, with the given first and last name.		
-def MakeNewCard(originPRN, firstName, lastName)
-	fields{'firstName': firstName, 'lastName': lastName}
+return children
+		
+#Create a new child card for the given parent account. Returns the Galileo ID.		
+def MakeNewCard(originPRN, firstName, lastName):
+	fields = {'firstName': firstName, 'lastName': lastName}
 	accountNo = CreateSecondaryAccount(originPRN, fields)
 	ActivateAccount(accountNo)
+	return accountNo
 
 #Simulate adding money to the primary account
-def AddPrimaryFunds(accountNo, amount)
+def AddPrimaryFunds(accountNo, amount):
 	CreateAdjustment(accountNo, amount)
 
 #Add money to the child account.
-def AddChildFunds(originPRN, targetPRN, amount)
+def AddChildFunds(originPRN, targetPRN, amount):
 	CreateAccountTransfer(originPRN, targetPRN, amount)
 
 #Returns true if card is frozen, false otherwise
-def GetFrozenStatus(accountNo)
-	#Get status from custom field, using API getUserDefinedAccountFields
+def GetFrozenStatusOfCard(accountNo):
+	return GetFrozenStatus(accountNo) == "1"
+	
 
 #Switch the status of the account between frozen and unfrozen.
-def switchFrozenStatus(accountNo)
-	if GetFrozenStatus(accountNo):
+def switchFrozenStatus(accountNo):
+	if GetFrozenStatus(accountNo) == "1":
 		UnfreezeAccount(accountNo)
 	else:
 		FreezeAccount(accountNo)
